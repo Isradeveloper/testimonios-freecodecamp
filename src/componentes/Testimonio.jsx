fc135ea4f2369a7f9c23deb98291c7e2ba8bbcff
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2022-09-09 16:43:52 -0500

    Creación del componente testimonios

---
+++ src/componentes/Testimonio.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Testimonio(){
+  return (
+    <div className="contenedor-testimonio">
+      <img src="" alt="testimonio" className="imagen-testimonio"/>
+    </div>
+  );
+}
